9575c8a7f8339b603f573519ca22e4e6df26cd78
Antonio David Mota <antoniodmota1996@gamil.com>
2018-03-05 15:32:43 +0100

    first commit

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,121 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Antonio David Mota Martinez on 3/3/18.
+//  Copyright © 2018 Antonio David Mota Martinez. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    //MARK: Properties
+    private var ratingButtons = [UIButton]()
+    
+    var rating = 0
+    
+ 
+    
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet {
+            setupButtons()
+        }
+    }
+    
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+        
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    //MARK: Button Action
+    @objc func ratingButtonTapped(button: UIButton){
+        //print("Button pressed 👍")
+        guard let index = ratingButtons.index(of: button) else {
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        
+        // Calculate the rating of the selected button
+        let selectedRating = index + 1
+        
+        if selectedRating == rating {
+            // If the selected star represents the current rating, reset the rating to 0.
+            rating = 0
+        } else {
+            // Otherwise set the rating to the selected star
+            rating = selectedRating
+        }
+    }
+    
+    //MARK: Private Methods
+    
+    private func updateButtonSelectionStates() {
+        
+    }
+    
+    
+    private func setupButtons() {
+        
+        // clear any existing buttons
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
+        // Load Button Images
+        let bundle = Bundle(for: type(of: self))
+        let filledStar = UIImage(named: "negra", in: bundle, compatibleWith: self.traitCollection)
+        let emptyStar = UIImage(named:"empty", in: bundle, compatibleWith: self.traitCollection)
+        let highlightedStar = UIImage(named:"azul", in: bundle, compatibleWith: self.traitCollection)
+        
+        for _ in 0..<starCount {
+            // Create the button
+            let button = UIButton()
+            // Set the button images
+            button.setImage(emptyStar, for: .normal)
+            button.setImage(filledStar, for: .selected)
+            button.setImage(highlightedStar, for: .highlighted)
+            button.setImage(highlightedStar, for: [.highlighted, .selected])
+            
+            // Add constraints
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+            
+            // Setup the button action
+             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            // Add the button to the stack
+            addArrangedSubview(button)
+            
+            // Add the new button to the rating button array
+            ratingButtons.append(button)
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
+}
---
+++ FoodTracker/UIStackView.swift
@@ -0,0 +1,13 @@
+//
+//  UIStackView.swift
+//  FoodTracker
+//
+//  Created by Antonio David Mota Martinez on 3/3/18.
+//  Copyright © 2018 Antonio David Mota Martinez. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    
+}
